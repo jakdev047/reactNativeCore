fae5f7d8778c06bbe9b441f4f9bb80f839d9f2a3
jakdev047 <jakfreelancer047@gmail.com>
2020-10-24 22:48:55 +0600

    Tab Navigation

--- src/components/NavigationTab/NavigationTab.js
+++ src/components/NavigationTab/NavigationTab.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SharePlaces from '../SharePlaces/SharePlaces';
+import FindPlaces from '../FindPlaces/FindPlaces';
+
+const Tab = createBottomTabNavigator();
 
 const NavigationTab = () => {
     return (
         <View>
-            <Text>
-                NavigationTab
-            </Text>
+            <Tab.Navigator>
+                <Tab.Screen name="Share Places" component={SharePlaces} />
+                <Tab.Screen name="Find Places" component={FindPlaces} />
+            </Tab.Navigator>
         </View>
     )
 };
