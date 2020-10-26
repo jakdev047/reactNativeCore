d21fcf6f94984c65788987f4f26982a49bcfebf4
jakdev047 <jakfreelancer047@gmail.com>
2020-10-26 23:16:17 +0600

    Using Google Firebase to Store Data

--- src/redux/actions/places.js
+++ src/redux/actions/places.js
@@ -1,10 +1,13 @@
 import { ADD_PLACE, DELETE_PLACE } from "./types";
 
-export const addPlace = place => {
-    return {
-        type: ADD_PLACE,
-        payload: place
-    }
+export const addPlace = place =>  dispatch => {
+    fetch("https://my-places-20029.firebaseio.com/places.json",{
+        method: "POST",
+        body: JSON.stringify(place)
+    })
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+    .catch(error=>console.log(error))
 };
 
 export const deletePlace = key => {
